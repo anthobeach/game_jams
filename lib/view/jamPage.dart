65d1ebf024647ca54652c5977d3e9045e645b4a5
anthobeach <anthonyjoguet97@gmail.com>
2024-03-25 09:32:36 +0100

    Ajout formulaire de connexion

--- lib/view/jamPage.dart
+++ lib/view/jamPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:carousel_slider/carousel_slider.dart";
 
 class JamPage extends StatefulWidget {
   const JamPage({super.key});
@@ -8,11 +9,25 @@ class JamPage extends StatefulWidget {
 }
 
 class _JamPageState extends State<JamPage> {
+  // create a list of images
+  List<Widget> carouselItems = [
+    Image.asset("assets/images/coverjams1.jpg"),
+    Image.asset("assets/images/coverjams2.jpg")
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Text('Jam Page'),
+      child: CarouselSlider(
+        items: carouselItems,
+        options : CarouselOptions(
+          height: double.infinity,
+          viewportFraction: 1.0,
+          autoPlay: true,
+          autoPlayCurve: Curves.easeInSine,
+          autoPlayAnimationDuration: const Duration(milliseconds: 500),
+        )
+        ),
     ));
   }
 }
---
+++ lib/view/sign_in_form.dart
@@ -0,0 +1,6 @@
+import "package:flutter/material.dart";
+import "package:supabase/supabase.dart";
+
+const String supabaseUrl = "https://wozhakobpiapbxstxmjd.supabase.co";
+const String apikey = 
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvemhha29icGlhcGJ4c3R4bWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEzMjAyNjksImV4cCI6MjAyNjg5NjI2OX0.qOyhIaTC18H6oUSD9sgNcZVU4oAFBRREIZrO703R6mo";
