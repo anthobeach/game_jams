3ee1128b437e8f99cb5e80070d170b996c8ed3fb
Kisald <nicobarreau178@laposte.net>
2024-03-25 19:07:00 +0100

    Ajout d'une page de login via l'exemple de la doc

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:GameJams/view/mainInterface.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() {
+  Supabase.initialize(
+    url: 'https://wozhakobpiapbxstxmjd.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvemhha29icGlhcGJ4c3R4bWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEzMjAyNjksImV4cCI6MjAyNjg5NjI2OX0.qOyhIaTC18H6oUSD9sgNcZVU4oAFBRREIZrO703R6mo',
+  );
+
   runApp(const MyApp());
 }
 
